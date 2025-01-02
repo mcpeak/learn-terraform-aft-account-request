9626f52f2d8e67abedadb45a0186e8d9c33fe77a
Travis McPeak <travis.mcpeak@resourcely.io>
2025-01-02 11:41:56 -0800

    Try again

---
+++ terraform/sandboxnew.tf
@@ -0,0 +1,19 @@
+module "newsandbox_fraMshbmm94dxGRa" {
+  source = "git::https://github.com/mcpeak/learn-terraform-aft-account-request//terraform/modules/aft-account-request"
+  account_tags = {
+    env_type = "prod"
+  }
+  change_management_parameters = {
+    change_requested_by = "Travis McPeak"
+    change_reason       = "I need a new account"
+  }
+  control_tower_parameters = {
+    AccountEmail              = "travis.mcpeak+sandboxnew@gmail.com"
+    AccountName               = "sandboxnew"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "travis.mcpeak@gmail.com"
+    SSOUserFirstName          = "Travis"
+    SSOUserLastName           = "McPeak"
+  }
+  account_customizations_name = "sandboxnew"
+}
