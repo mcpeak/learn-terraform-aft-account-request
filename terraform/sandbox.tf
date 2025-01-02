03ddc2181918ae5db4143a45ebf150b27debd48b
Travis McPeak <travis.mcpeak@resourcely.io>
2025-01-02 09:56:54 -0800

    Update sandbox

--- terraform/sandbox.tf
+++ terraform/sandbox.tf
@@ -4,8 +4,8 @@ module "sandbox_fraMshbmm94dxGRa" {
     env_type = "prod"
   }
   change_management_parameters = {
-    change_requested_by   = "Travis McPeak"
-    change_request_reason = "I need a new account"
+    change_requested_by = "Travis McPeak"
+    change_reason       = "I need a new account"
   }
   control_tower_parameters = {
     AccountEmail              = "travis.mcpeak+new_account_test@gmail.com"
